a2254c04d3342c63cf9ba773a64851b49f63f594
Caoyuan Deng <dcaoyuan@gmail.com>
2015-01-13 15:26:56 +0800

    Moved out case class of primitive APIs from avpath

--- src/main/scala/wandou/avpath/package.scala
+++ src/main/scala/wandou/avpath/package.scala
@@ -1,30 +1,8 @@
 package wandou
 
-import org.apache.avro.generic.GenericData.Record
 import org.apache.avro.generic.IndexedRecord
 
 package object avpath {
-
-  trait Command extends Serializable {
-    def id: String
-  }
-
-  final case class Select(id: String, path: String) extends Command
-  final case class Update(id: String, path: String, value: Any) extends Command
-  final case class UpdateJson(id: String, path: String, value: String) extends Command
-  final case class Insert(id: String, path: String, value: Any) extends Command
-  final case class InsertJson(id: String, path: String, value: String) extends Command
-  final case class InsertAll(id: String, path: String, values: List[_]) extends Command
-  final case class InsertAllJson(id: String, path: String, values: String) extends Command
-  final case class Delete(id: String, path: String) extends Command
-  final case class Clear(id: String, path: String) extends Command
-
-  final case class GetRecord(id: String) extends Command
-  final case class PutRecord(id: String, record: Record) extends Command
-  final case class PutRecordJson(id: String, record: String) extends Command
-  final case class GetField(id: String, field: String) extends Command
-  final case class PutField(id: String, field: String, value: Any) extends Command
-  final case class PutFieldJson(id: String, field: String, value: String) extends Command
 
   def select(data: IndexedRecord, path: String): List[Any] = {
     val p = new Parser()
