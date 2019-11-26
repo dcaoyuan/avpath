ec5ec201c0393146d44161fcbad2378a824c2fc3
Pierre Bailly <pbailly@talend.com>
2019-11-26 11:09:33 +0100

    chore(release): Put back a snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,7 @@ object Build extends sbt.Build {
 
   lazy val basicSettings = Seq(
     organization := "com.wandoulabs.avro",
-    version := "0.1.5-talend",
+    version := "0.1.6-talend-SNAPSHOT",
     scalaVersion := "2.11.12",
     scalacOptions ++= Seq("-unchecked", "-deprecation"),
     resolvers ++= Seq(
